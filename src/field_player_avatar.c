631cc713bf60daaf76703d209d2944e58c5e0e41
CeeJiggy <92240739+CeeJiggy@users.noreply.github.com>
2024-01-20 11:54:32 -0500

    Remove dexnav flags

--- src/field_player_avatar.c
+++ src/field_player_avatar.c
@@ -628,7 +628,7 @@ static void PlayerNotOnBikeMoving(u8 direction, u16 heldKeys)
     if (gPlayerAvatar.flags & PLAYER_AVATAR_FLAG_SURFING)
     {
         // same speed as running
-        PlayerWalkFast(direction);
+        PlayerWalkFaster(direction);
         return;
     }
 
